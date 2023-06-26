afffaf316cedf815c4da420a9bffae8d0420182c
Elpluribusunum <iyogunclarkson@gmail.com>
2023-06-25 21:58:02 -0700

    yhhh

---
+++ queue.c
@@ -0,0 +1,48 @@
+#include "monty.h"
+/**
+ * f_queue - images on the top
+ * @head: pile head
+ * @counter: Line_number
+ * Return: the end
+*/
+void f_queue(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 1;
+}
+
+/**
+ * addqueue - Node to the tail stack be added
+ * @n: New_value
+ * @head: front of the line
+ * Return: the end
+*/
+void addqueue(stack_t **head, int n)
+{
+	stack_t *new_node, *aux;
+
+	aux = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (aux)
+	{
+		while (aux->next)
+			aux = aux->next;
+	}
+	if (!aux)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		aux->next = new_node;
+		new_node->prev = aux;
+	}
+}
--- stack.c
+++ stack.c
@@ -1,73 +1,13 @@
 #include "monty.h"
-#include <string.h>
-
-
-void free_stack(stack_t **stack);
-int init_stack(stack_t **stack);
-int check_mode(stack_t *stack);
-
-
 /**
- * free_stack - sets a stack_t stack free.
- * @stack: a pointer to the stack or queue at
- * the top or bottom of a stack_t.
- */
-void free_stack(stack_t **stack)
+ * f_stack - images on the top
+ * @head: pile head
+ * @counter: line_number
+ * Return: the end
+*/
+void f_stack(stack_t **head, unsigned int counter)
 {
-	stack_t *tmp = *stack;
-
-
-	while (*stack)
-	{
-		tmp = (*stack)->next;
-		free(*stack);
-		*stack = tmp;
-	}
-}
-
-
-/**
- * init_stack - creates beginning stack and finishing queue
- * nodes for a stack_t stack.
- * @stack: a unitialized stack_t stack pointer.
- *
- * Return: The error code is EXIT_FAILURE.
- * If not, EXIT_SUCCESS.
- */
-int init_stack(stack_t **stack)
-{
-	stack_t *s;
-
-
-	s = malloc(sizeof(stack_t));
-	if (s == NULL)
-		return (malloc_error());
-
-	s->n = STACK;
-	s->prev = NULL;
-	s->next = NULL;
-
-	*stack = s;
-
-
-	return (EXIT_SUCCESS);
-}
-
-
-/**
- * check_mode - dmi whth a linked stack of type stack is in stack or queue mode
- * @stack: a pointing device for a stack_t linked
- * list's top (stack) or bottom (queue).
- *
- * Return: Stack(0) if the stack_t is in stack mode.
- * QUEUE (1) if the stack_t is in queue mode.
- * Without it, 2.
- */
-int check_mode(stack_t *stack)
-{
-	if (stack->n == STACK)
-		return (STACK);
-	else if (stack->n == QUEUE)
-		return (QUEUE);
-	return (2);
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
 }
